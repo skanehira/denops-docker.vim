8767e206383f2ce1e2f2b621413ae74734cc158c
skanehira <sho19921005@gmail.com>
2022-05-11 22:10:57 +0900

    refactor: add task.sh

---
+++ task.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+container_api="denops-mock-api"
+container_network="denops-mock-net"
+
+function isNetworkExist() {
+  id=$(docker network ls --filter name=denops-mock-net --format "{{.ID}}")
+  if [ "${id}" != "" ];then
+    echo true
+  else
+    echo false
+  fi
+}
+
+function isContainerRunning() {
+  state=$(docker ps -a -f "name=^${1}$" --format "{{.State}}")
+  if [ "${state}" = "running" ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+function isContainerExist() {
+  state=$(docker ps -a -f "name=^${1}$" --format "{{.State}}")
+  if [ "${state}" != "" ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+function removeContainers() {
+  for name in $@; do
+    if ! $(isContainerExist $name); then
+      continue
+    fi
+    if $(isContainerRunning $name); then
+      docker kill ${name} && sleep 0.5
+    fi
+    docker rm ${name}
+  done
+}
+
+function doUp() {
+  if ! $(isNetworkExist $container_network); then
+    echo "create container network: ${container_network}"
+    docker network create ${container_network}
+  fi
+
+  if ! $(isContainerExist $container_api); then
+    echo "start container: ${container_api}"
+    docker run --rm -v ${PWD}:/mock --name ${container_api} \
+      --net ${container_network} \
+      -p 9999:4010 \
+      -d stoplight/prism mock \
+      -h 0.0.0.0 /mock/swagger.yaml
+    docker run --rm --net $container_network jwilder/dockerize -timeout 180s -wait http://${container_api}:4010/containers/json
+  fi
+}
+
+function doDown() {
+  if $(isContainerExist $container_api); then
+    docker kill ${container_api}
+  fi
+
+  if $(isNetworkExist $container_network); then
+    docker network rm ${container_network}
+  fi
+}
+
+function doTest() {
+  # TODO enable type check
+  # https://github.com/keroxp/servest/issues/170
+  deno test -A --unstable --coverage=cov --no-check=remote "https://deno.land/x/servest@v1.3.4/testing.ts" .
+  case $1 in
+    cov)
+      deno coverage cov
+      rm -rf cov
+      ;;
+    *)
+      ;;
+  esac
+}
+
+function main() {
+  case $1 in
+    test)
+      doUp
+      doTest $2
+      ;;
+    mock:api:up)
+      doUp
+      ;;
+    mock:api:down)
+      doDown
+      ;;
+    *)
+      echo -e "invalid argument"
+      ;;
+  esac
+}
+
+
+main $@
