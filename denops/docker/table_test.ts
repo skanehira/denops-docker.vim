f78029d99c7ca95682a98376a47f9eb2e4bf3317
skanehira <sho19921005@gmail.com>
2022-05-12 08:14:14 +0900

    refactor: run tests in block scope

--- denops/docker/table_test.ts
+++ denops/docker/table_test.ts
@@ -3,34 +3,36 @@ import { Image } from "./types.ts";
 import { assertEquals } from "./deps.ts";
 import { readFile, readJSON } from "./testutil.ts";
 
-const tests = [
-  {
-    kind: "images",
-    dataFile: "denops/docker/testdata/table/images.json",
-    wantFile: "denops/docker/testdata/table/images.out",
-  },
-  {
-    kind: "containers",
-    dataFile: "denops/docker/testdata/table/containers.json",
-    wantFile: "denops/docker/testdata/table/containers.out",
-  },
-  {
-    kind: "searchImages",
-    dataFile: "denops/docker/testdata/table/searchImages.json",
-    wantFile: "denops/docker/testdata/table/searchImages.out",
-  },
-];
+{
+  const tests = [
+    {
+      kind: "images",
+      dataFile: "denops/docker/testdata/table/images.json",
+      wantFile: "denops/docker/testdata/table/images.out",
+    },
+    {
+      kind: "containers",
+      dataFile: "denops/docker/testdata/table/containers.json",
+      wantFile: "denops/docker/testdata/table/containers.out",
+    },
+    {
+      kind: "searchImages",
+      dataFile: "denops/docker/testdata/table/searchImages.json",
+      wantFile: "denops/docker/testdata/table/searchImages.out",
+    },
+  ];
 
-tests.forEach((test) => {
-  Deno.test(test.kind, async () => {
-    const images = await readJSON<Image[]>(
-      test.dataFile,
-    );
-    const table = makeTableString(images, test.kind as TableKind);
-    const got = table;
+  for (const test of tests) {
+    Deno.test(test.kind, async () => {
+      const images = await readJSON<Image[]>(
+        test.dataFile,
+      );
+      const table = makeTableString(images, test.kind as TableKind);
+      const got = table;
 
-    const want = await readFile(test.wantFile);
+      const want = await readFile(test.wantFile);
 
-    assertEquals(got, want.split("\n"));
-  });
-});
+      assertEquals(got, want.split("\n"));
+    });
+  }
+}
