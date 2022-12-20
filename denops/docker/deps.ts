4f16922e4e286a2917e85de5aa713a45c04cd110
denopendabot <denopendabot@github.com>
2022-12-20 00:18:42 +0000

    build(deps): bump denops_std from v3.8.2 to v3.12.0

--- denops/docker/deps.ts
+++ denops/docker/deps.ts
@@ -1,12 +1,12 @@
-export * from "https://deno.land/x/denops_std@v3.8.2/mod.ts";
+export * from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
 export { isString } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.8.2/autocmd/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts";
 export { Table } from "https://deno.land/x/cliffy@v0.25.1/table/mod.ts";
 export { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
 export { datetime } from "https://deno.land/x/ptera@v1.0.2/mod.ts";
-export * as mapping from "https://deno.land/x/denops_std@v3.8.2/mapping/mod.ts";
-export * as mapType from "https://deno.land/x/denops_std@v3.8.2/mapping/types.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.8.2/variable/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.12.0/mapping/mod.ts";
+export * as mapType from "https://deno.land/x/denops_std@v3.12.0/mapping/types.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.12.0/variable/mod.ts";
 export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
 export * as fs from "https://deno.land/std@0.170.0/fs/mod.ts";
 export { open } from "https://deno.land/x/open@v0.0.5/index.ts";
